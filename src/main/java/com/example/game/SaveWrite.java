9310433cef4a714995f1921760d6b8da1eca8563
nieweilin11 <fishonlinenwl@gmail.com>
2022-04-13 21:45:19 +0200

    initial

--- src/main/java/com/example/game/SaveWrite.java
+++ src/main/java/com/example/game/SaveWrite.java
@@ -1,5 +1,32 @@
 package com.example.game;
 
+import java.io.*;
+
 public class SaveWrite {
 
+    public void Writhe (){
+        //要写入的数据
+
+/*
+
+        File f = new File("/player.json");
+        //将数据写入.json文件--start
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f,false), "UTF-8"));
+            writer.write(jsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(writer != null){
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+*/
+
+    }
 }
