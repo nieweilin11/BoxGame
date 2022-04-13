e9bcc07f877d9ba0811777f3f5169117caed287f
nieweilin11 <fishonlinenwl@gmail.com>
2022-04-13 21:01:19 +0200

    initial "Test classes"

---
+++ src/test/java/com/example/game/JudgeStateTest.java
@@ -0,0 +1,22 @@
+package com.example.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JudgeStateTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void judgePlayerMovement() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updatePuzzle() {
+    }
+}
---
+++ src/test/java/com/example/game/RoundInfoTest.java
@@ -0,0 +1,24 @@
+package com.example.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoundInfoTest {
+
+    @Test
+    void playerMovement() {
+    }
+
+    @Test
+    void roundCounter() {
+    }
+
+    @Test
+    void exitAndSave() {
+    }
+
+    @Test
+    void scoreTable() {
+    }
+}
