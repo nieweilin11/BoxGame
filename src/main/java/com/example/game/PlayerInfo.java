3a4cac23e9ce881ee820a4a43935e7878bbd2e12
nieweilin11 <fishonlinenwl@gmail.com>
2022-04-28 15:41:44 +0200

    Updated GUI and  move some methods into "RoundInfo","PlayerInfo" class

--- src/main/java/com/example/game/PlayerInfo.java
+++ src/main/java/com/example/game/PlayerInfo.java
@@ -78,7 +78,5 @@ public class PlayerInfo {
         this.playerName = playerName;
     }
 
-    public String getPlayerName() {
-        return playerName;
-    }
+
 }
--- src/main/java/com/example/game/RoundInfo.java
+++ src/main/java/com/example/game/RoundInfo.java
@@ -3,6 +3,7 @@ package com.example.game;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
