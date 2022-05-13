e035c08ff42cfb00d81ff87a508782e6fe0374d2
nieweilin11 <fishonlinenwl@gmail.com>
2022-05-13 23:01:15 +0200

    connect playerStep to JavaFx to display dynamic change

--- src/main/java/GameView/Main.java
+++ src/main/java/GameView/Main.java
@@ -1,10 +1,10 @@
 package GameView;
 
+import Controller.PlayerController;
+import Controller.RoundController;
 import Controller.SaveController;
 import Model.Player;
 import Model.Round;
-import Controller.PlayerController;
-import Controller.RoundController;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -20,7 +20,6 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Polyline;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import org.tinylog.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,10 +40,8 @@ public class Main extends Application {
     private final SaveController saveController = SaveController.getSaveController();
     private final RoundController roundController = RoundController.getRoundController();
 
-
-    private  List <Circle>puzzleList= roundController.getPuzzleList();
+    private final List <Circle>puzzleList= roundController.getPuzzleList();
     private final List <Polyline>boxList =roundController.getBoxsList();
-
 
     @Override
     public void start(Stage primaryStage) {
@@ -52,138 +49,107 @@ public class Main extends Application {
         /*
           instantiate nodes
          */
-        int line=11;
+        int line = 11;
         ArrayList<Line> lList = new ArrayList<>();
-        for (int i=0;i<line;i++){
-            lList.add(new Line(0,46*i,200,46*i));
+        for (int i = 0; i < line; i++) {
+            lList.add(new Line(0, 46 * i, 200, 46 * i));
         }
-        Line line1=new Line(30,0,30,500);
+        Line line1 = new Line(30, 0, 30, 500);
         lList.add(line1);
-        ArrayList<Label>rank=new ArrayList<>();
-        for (var i=0;i<line;i++){
-            String text=""+i;
+        ArrayList<Label> rank = new ArrayList<>();
+        for (var i = 0; i < line; i++) {
+            String text = "" + i;
             rank.add(new Label(text));
-            setLabelPosition(rank.get(i),4,46*i);
+            setLabelPosition(rank.get(i), 4, 46 * i);
             rank.get(i).setFont(new Font(26));
         }
-        Button newGame=new Button("Start New Game");
-        Button score =new Button("Rank");
-        Button aboutMe=new Button("About Me");
-        Button loadGame =new Button("Load a Game");
-        Button save =new Button("Save");
-        Button homeGame =new Button("Home");
-        Button homeLoad =new Button("Home");
-        Button homeNew =new Button("Home");
-        Button loadConfirm=new Button("confirm");
-        Button newConfirm=new Button("confirm");
-        Label homeTitle=new Label("BoxGame");
-        Label roundS=new Label("Round :");
-        homeTitle.setTextFill(Color.rgb(0,0,0));
+
+        Button newGame = new Button("Start New Game");
+        Button score = new Button("Rank");
+        Button aboutMe = new Button("About Me");
+        Button loadGame = new Button("Load a Game");
+        Button save = new Button("Save");
+        Button homeGame = new Button("Home");
+        Button homeLoad = new Button("Home");
+        Button homeNew = new Button("Home");
+        Button loadConfirm = new Button("confirm");
+        Button newConfirm = new Button("confirm");
+        Label homeTitle = new Label("BoxGame");
+        Label roundS = new Label("Round : "+roundController.getRoundCounter());
+        homeTitle.setTextFill(Color.rgb(0, 0, 0));
         homeTitle.setFont(new Font(50));
         roundS.setFont(new Font(15));
-        TextField saveFile=new TextField();
-        TextField name=new TextField();
+        TextField saveFile = new TextField();
+        TextField name = new TextField();
+        if (roundController.isPassSelect()){
+            roundS.setText("Round : "+roundController.getRoundCounter());
+            roundController.setFlash(false);
+        }
 
         /*
           set buttons,textFiled and labors
          */
-        setButtonPosition(save,880,450);
-        setButtonPosition(aboutMe,200,450);
-        setButtonPosition(homeLoad,400,450);
-        setButtonPosition(homeNew,400,450);
-        setButtonPosition(loadGame,400,450);
-        setButtonPosition(score,935,420);
-        setTextFile(saveFile,150,200);
-        setTextFile(name,150,200);
-        setButtonPosition(loadConfirm,250,220);
-        setButtonPosition(newConfirm,250,220);
-        setButtonPosition(newGame,300,300);
-        setButtonPosition(loadGame,100,300);
-        setButtonPosition(homeGame,930,450);
-        setLabelPosition(roundS,10,10);
-        setLabelPosition(homeTitle,30,195);
-        setLabelSize(homeTitle,80,400);
+        setButtonPosition(save, 880, 450);
+        setButtonPosition(aboutMe, 200, 450);
+        setButtonPosition(homeLoad, 400, 450);
+        setButtonPosition(homeNew, 400, 450);
+        setButtonPosition(loadGame, 400, 450);
+        setButtonPosition(score, 935, 420);
+        setTextFile(saveFile, 150, 200);
+        setTextFile(name, 150, 200);
+        setButtonPosition(loadConfirm, 250, 220);
+        setButtonPosition(newConfirm, 250, 220);
+        setButtonPosition(newGame, 300, 300);
+        setButtonPosition(loadGame, 100, 300);
+        setButtonPosition(homeGame, 930, 450);
+        setLabelPosition(roundS, 10, 10);
+        setLabelPosition(homeTitle, 30, 195);
+        setLabelSize(homeTitle, 80, 400);
         homeTitle.setAlignment(Pos.CENTER);
 
         /*
           instantiate panes and set panes
          */
-        AnchorPane homePane =new AnchorPane();
-        AnchorPane gamePane =new AnchorPane();
-        AnchorPane loadPane =new AnchorPane();
-        AnchorPane newGamePane =new AnchorPane();
-        AnchorPane scorePane =new AnchorPane();
+        AnchorPane homePane = new AnchorPane();
+        AnchorPane gamePane = new AnchorPane();
+        AnchorPane loadPane = new AnchorPane();
+        AnchorPane newGamePane = new AnchorPane();
+        AnchorPane scorePane = new AnchorPane();
         homePane.setStyle("-fx-background-image: url(" + "file:src/main/resources/Image/red.jpg" + "); " +
-                        "-fx-background-position: center center; " +
-                        "-fx-background-repeat: stretch;" +
-                        "-fx-background-color:  transparent;");
+                "-fx-background-position: center center; " +
+                "-fx-background-repeat: stretch;" +
+                "-fx-background-color:  transparent;");
         gamePane.setStyle("-fx-background-image: url(" + "file:src/main/resources/Image/game.jpg" + "); " +
-                        "-fx-background-position: center center; " +
-                        "-fx-background-repeat: stretch;" +
-                        "-fx-background-color:  transparent;");
+                "-fx-background-position: center center; " +
+                "-fx-background-repeat: stretch;" +
+                "-fx-background-color:  transparent;");
         loadPane.setStyle("-fx-background-image: url(" + "file:src/main/resources/Image/black.jpg" + "); " +
-                        "-fx-background-position: center center; " +
-                        "-fx-background-repeat: stretch;" +
-                        "-fx-background-color:  transparent;");
+                "-fx-background-position: center center; " +
+                "-fx-background-repeat: stretch;" +
+                "-fx-background-color:  transparent;");
         newGamePane.setStyle("-fx-background-image: url(" + "file:src/main/resources/Image/black.jpg" + "); " +
-                        "-fx-background-position: center center; " +
-                        "-fx-background-repeat: stretch;" +
-                        "-fx-background-color:  transparent;");
+                "-fx-background-position: center center; " +
+                "-fx-background-repeat: stretch;" +
+                "-fx-background-color:  transparent;");
         /*
           instantiate scenes
          */
-        Scene primaryScene =new Scene(homePane,500,500);
-        Scene gameScene=new Scene(gamePane,1000,500);
-        Scene loadScene=new Scene(loadPane,500,500);
-        Scene newGameScene=new Scene(newGamePane,500,500);
-        Scene scoreScene=new Scene(scorePane,200,500);
+        Scene primaryScene = new Scene(homePane, 500, 500);
+        Scene gameScene = new Scene(gamePane, 1000, 500);
+        Scene loadScene = new Scene(loadPane, 500, 500);
+        Scene newGameScene = new Scene(newGamePane, 500, 500);
+        Scene scoreScene = new Scene(scorePane, 200, 500);
         /*
           set red and black stone  and emptyBox click event
-         */        for (Circle circle : puzzleList) {
-            gamePane.getChildren().add(circle);
-        }
-        for (int i=0;i<round.getPlayerStep().size();i++) {
-            int finalI = i;
-            if (!roundController.isChessMax()) {
-                puzzleList.get(i).setOnMouseClicked(mouseEvent -> {
-                    if (puzzleList.get(finalI).getLayoutY() > 180) {
-                        puzzleList.get(finalI).setLayoutY(180);
-                        roundController.roundCounter(1);
-                        selected(finalI,-1);
-                        Logger.trace(puzzleList);
-                    }
-                    else {
-                        puzzleList.get(finalI).setLayoutY(220);
-                        selected(finalI,1);
-                    }
-                    roundController.roundCounter(-1);
-                    if (roundController.isReset()){
-                        for (int j=0;j<roundController.totalBox;j++) {
-                            puzzleList.get(j).setLayoutY(220);
-                        }
-                        roundController.roundCounter(-1);
-                        roundController.setReset(false);
-                        Logger.trace("Set Reset false");
-                    }
-                    if (roundController.isPassSelect()){
-                        roundController.displayStones(round.getPlayerStep());
-                        Logger.trace(puzzleList);
-                        roundController.setPassSelect(false);
-                        for (int j=0;j<puzzleList.size();j++) {
-                            gamePane.getChildren().set(j,puzzleList.get(j));
-                        }
-                    }
-                    /*convert(round.getPlayerStep(),puzzleList);*/
-                });
-            }
-        }
+         */
+
 
         /*
           set textFile obtains files absolute address event
          */
         saveFile.setOnDragOver(dragEvent -> dragEvent.acceptTransferModes(TransferMode.ANY));
         saveFile.setOnDragDropped(dragEvent -> {
-            dragEvent.getDragboard();
+           /* dragEvent.getDragboard();*/
             if (dragEvent.getDragboard().hasFiles()) {
                 String path = dragEvent.getDragboard().getFiles().get(0).getAbsolutePath();
                 saveFile.setText(path);
@@ -198,6 +164,7 @@ public class Main extends Application {
         /*
           set buttons click events
          */
+        selectEvent();
         newConfirm.setOnAction(actionEvent -> {
             player.setPlayerName(name.getText());
             System.out.println("name:" + player.getPlayerName());
@@ -216,44 +183,49 @@ public class Main extends Application {
         loadGame.setOnAction(actionEvent -> primaryStage.setScene(loadScene));
         homeLoad.setOnAction(actionEvent -> primaryStage.setScene(primaryScene));
         score.setOnAction(actionEvent -> {
-            Stage scoreStage =new Stage();
+            Stage scoreStage = new Stage();
             scoreStage.setHeight(500);
             scoreStage.setWidth(200);
             scoreStage.setScene(scoreScene);
             scoreStage.show();
         });
+
         /*
           add nodes into pane
          */
-        homePane.getChildren().addAll(newGame,loadGame,homeTitle,aboutMe);
-        loadPane.getChildren().addAll(homeLoad,loadConfirm,saveFile);
-        newGamePane.getChildren().addAll(newConfirm,homeNew,name);
-        gamePane.getChildren().addAll(save,homeGame,roundS,score);
+            homePane.getChildren().addAll(newGame, loadGame, homeTitle, aboutMe);
+            loadPane.getChildren().addAll(homeLoad, loadConfirm, saveFile);
+            newGamePane.getChildren().addAll(newConfirm, homeNew, name);
+            gamePane.getChildren().addAll(save, homeGame, roundS, score);
 
-        for (var i=1;i<boxList.size();i++){
-            gamePane.getChildren().add(boxList.get(i));
-        }
-        for (Line value : lList) {
-            scorePane.getChildren().addAll(value);
-        }
-        for (Label label : rank) {
-            scorePane.getChildren().add(label);
-        }
+            for (var i = 1; i < boxList.size(); i++) {
+                gamePane.getChildren().add(boxList.get(i));
+            }
+
+            for (Line value : lList) {
+                scorePane.getChildren().addAll(value);
+            }
+            for (Label label : rank) {
+                scorePane.getChildren().add(label);
+            }
+            for (Circle circle : puzzleList) {
+                gamePane.getChildren().add(circle);
+            }
+
         /*
           add panes into stage and set stage
          */
-        primaryStage.setScene(primaryScene);
-        primaryStage.setTitle("BoxGame");
-        primaryStage.centerOnScreen();
-        primaryStage.setY(primaryStage.getY() * 3f / 2f);
-        primaryStage.setResizable(false);
-        primaryStage.getIcons().add(new Image("C:\\Users\\Fish\\IdeaProjects\\Game\\src\\main\\resources\\Image\\29x29.jpg"));
-        primaryStage.show();
-        primaryStage.setOnCloseRequest(event -> {
-            System.out.print("Windows shut down");
-            roundController.endTime();
-            /*saveController.write();*/ //json file don't find now
-        });
+            primaryStage.setScene(primaryScene);
+            primaryStage.setTitle("BoxGame");
+            primaryStage.centerOnScreen();
+            primaryStage.setY(primaryStage.getY() * 3f / 2f);
+            primaryStage.setResizable(false);
+            primaryStage.getIcons().add(new Image("C:\\Users\\Fish\\IdeaProjects\\Game\\src\\main\\resources\\Image\\29x29.jpg"));
+            primaryStage.show();
+            primaryStage.setOnCloseRequest(event -> {
+                roundController.endTime();
+                /*saveController.write();*/ //json file don't find now
+            });
     }
 
     public static void main(String[] args) {launch();}
